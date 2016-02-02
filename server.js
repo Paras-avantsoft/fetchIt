8a8d52e3a0f7dddfc9437b06c10de23d5b7d9c87
Paras-avantsoft <paras.avantsoft@gmail.com>
2016-02-02 15:59:48 +0530

    "Reading txt file and fetching contacts and email"

--- server.js
+++ server.js
@@ -1,32 +1,39 @@
 var http = require('http');
-var https = require('https');
 var url = require('url');
 var fs = require('fs');
 var path = require('path');
-var cse_token = '008171334859300485105:xjnxxb8dxnk';
-var g_api_key = 'AIzaSyDNUZTem7yknzqZuWrJfOGmDUERMi1J6KA';
-var no_items = 10;
+var emailRegex = /(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/g;
+var phoneRegex = /([+\s]\d{12}|[+\.\s]\d{1}[-\.\s]\d{10}|[+\.\s]\d{2}[-\.\s]\d{10}|\d{3}[-\.\s]\d{3}[-\.\s]\d{4}|\d{2}[-\.\s]\d{3}[-\.\s]\d{5}|\d{3}[-\.\s]\d{7}|\d{10}|\(\d{3}\)\s*\d{3}[-\.\s]\d{4}|\d{3}[-\.\s]\d{4})/g;
 
 http.createServer(function(req, res) {
-     console.log("url >>>", req.url);
-    var uId = url.parse(req.url,true).query.uId,
-        obj = '';
+    var phone = [],
+        email = [],
+        json = {
+            'phone': {
+            },
+            'email': {
+            }
+        },
+        i = 0;
 
-    console.log("uId >>>", uId);
-    console.log("exists >>>>", fs.existsSync("./src/vcard.json"));
-    fs.readFile('./src/vcard.json', 'utf8', function (err, data) {
+    fs.readFile('./output.txt', 'utf8', function (err, data) {
         if (err) throw err;
-        obj = JSON.parse(data);
-        console.log(obj);
+        phone = data.match(phoneRegex);
+        email = data.match(emailRegex);
+
+        for (var i = 0; i < phone.length; i++) {
+            json.phone['tel'+(i+1)] = phone[i];
+        };
+        for (var i = 0; i < email.length; i++) {
+            json.email['email'+(i+1)] = email[i];
+        };
+        console.log(json);
     });
-    var jsonData = vcard.convertJsonToVCF(obj);
-    fs.writeFile(__dirname + '/' + uId +'-myvcard.vcf', jsonData, function(err){
-        if (err) throw err;
-    });
+
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader('Access-Control-Allow-Headers', '*');
     res.setHeader('200',{"Content-Type":"text/vcard"});
-    res.write('data reader from file');
+    res.write('fetching data from file...');
     res.end();
 }).listen(4786);
